912376bd9b506181bce67f4eb2e65267bba291cf
Aadinir <aadiniranjan27@gmail.com>
2024-06-07 16:29:37 +0530

    new commit

--- src/app/emailclass/page.tsx
+++ src/app/emailclass/page.tsx
@@ -1,17 +1,29 @@
 "use client"
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Emailclass() {
+    const[count,setcount]=useState(0);
   async function login() {
     let code = localStorage.getItem('oauth-token')
     try {
-      const response = await axios.get(`http://localhost:3000/api/v1/emailclassifier/oauth2callback?code=${code}`)
-      console.log(response)
+        if(count ==0){
+            await axios.get('http://localhost:3000/newoauthcallbacknew', {
+                withCredentials: true // Include credentials (cookies) in the request
+              })
+        }
+    //   const response = await axios.get(`http://localhost:3000/api/v1/emailclassifier/oauth2callback?code=${code}`)
+    //   console.log(response)
       // The server will redirect, so no further action is needed here
     } catch (error) {
       console.error('Error during login:', error);
     }
+  }
+  async function fetchemail(val:number){
+    const resp = await axios.get(`http://localhost:3000/api/v1/emailclassifier/oauth2callback?count=${val}`, {
+        withCredentials: true // Include credentials (cookies) in the request
+      });
+    console.log(resp)
   }
 
 //   useEffect(() => {
@@ -21,6 +33,7 @@ export default function Emailclass() {
     <div>
       <h1>Logging in...</h1>
       <button onClick={login}>hoijsidwjiowi</button>
+      <button onClick={()=>fetchemail(20)}>Get me the subject</button>
     </div>
   );
 }
